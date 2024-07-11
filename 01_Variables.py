a57f479881b69ab8e080b1613dba4c0fdd67dfda
Blamcho <94477654+Blamcho@users.noreply.github.com>
2024-07-10 22:11:08 -0600

    print y variables
    Primeras practicas  print y variables

---
+++ 01_Variables.py
@@ -0,0 +1,17 @@
+#varilabeles 
+my_variable = "My variable"
+print (my_variable)
+
+variable_int = 5
+print (variable_int)
+
+variable_int_to_str_variable = str(variable_int)
+print (variable_int_to_str_variable)
+print (type(variable_int_to_str_variable))
+
+variable_bool = False
+print (variable_bool)
+
+
+# concatenacion de variables en un print
+print(my_variable, str (variable_int), variable_bool)
